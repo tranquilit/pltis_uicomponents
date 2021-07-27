aebc129d4ff0db8a6f0ef29e51cee415d248ccdf
Marcos Douglas <msantos@tranquil.it>
2021-07-27 12:37:36 -0300

    renamed package from tis.ui.components to pltis_uicomponents - it seems lazbuild do not support dotted package names :(

---
+++ pack/pltis_uicomponents.pas
@@ -0,0 +1,24 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit pltis_uicomponents;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  tis.ui.registry, tis.ui.grid.core, tis.ui.grid.editor, tis.core.os, 
+  tis.core.utils, tis.ui.searchedit, tis.ui.tageditor, tis.ui.togglebutton, 
+  LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+  RegisterUnit('tis.ui.registry', @tis.ui.registry.Register);
+end;
+
+initialization
+  RegisterPackage('pltis_uicomponents', @Register);
+end.
