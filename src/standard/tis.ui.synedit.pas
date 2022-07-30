235a56b6d906d2467f65329f033de4297c6df816
Marcos Douglas B. Santos <msantos@tranquil.it>
2022-07-30 09:03:09 -0300

    synedit: introduced TTisSynEdit

---
+++ src/standard/tis.ui.synedit.pas
@@ -0,0 +1,26 @@
+// -----------------------------------------------------------------
+//    This file is part of Tranquil IT Software
+//    Copyright (C) 2012 - 2022  Tranquil IT https://www.tranquil.it
+//    All Rights Reserved.
+// ------------------------------------------------------------------
+unit tis.ui.synedit;
+
+{$i tis.ui.defines.inc}
+
+interface
+
+uses
+  Classes, 
+  SysUtils,
+  Forms, 
+  Controls, 
+  Graphics, 
+  SynEdit;
+
+type
+  TTisSynEdit = class(TSynEdit)
+  end;
+
+implementation
+
+end.
