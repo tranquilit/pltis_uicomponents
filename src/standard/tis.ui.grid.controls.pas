tranquilit/pltis_uicomponents

commit 2a5f9400951f96f5862ce0016ea9bd699b88c9fd

grid: fixed hint on GetValueAsString

src/standard/tis.ui.grid.core.pas
@@ -2003,7 +2003,7 @@ function TTisNodeAdapter.GetValueAsString(aNode: PVirtualNode;
   function LJsonVarToStr(const aValue: Variant): string;
   begin
     if VarIsBool(aValue) then
-      result := LowerCase(aValue)
+      result := Utf8ToString(LowerCase(VariantToUtf8(aValue)))
     else
       result := Utf8ToString(VariantToUtf8(aValue));
   end;
@@ -2016,7 +2016,6 @@ begin
     result := LJsonVarToStr(vValue^)
   else
     result := aDefault;
-  result := Utf8ToString(result);
 end;
 
 procedure TTisNodeAdapter.SetValue(aNode: PVirtualNode; const aValue: Variant;

commit 218705810b6449be4ba4c5aa01bb21cea300cd65

grid: fixed raw JSON editing - when in TreeMode/Children Mode - bug introduced in 2a5f9400

src/standard/tis.ui.grid.controls.pas
@@ -244,7 +244,10 @@ end;
 
 procedure TTisGridEditControl.SetValue(const aValue: Variant);
 begin
-  Edit.Text := VarToStr(aValue);
+  if VarIsBool(aValue) then
+    Edit.Text := LowerCase(aValue)
+  else
+    Edit.Text := VarToStr(aValue);
 end;
 
 function TTisGridEditControl.Edit: TEdit;

src/standard/tis.ui.grid.core.pas
@@ -2004,7 +2004,7 @@ function TTisNodeAdapter.GetValueAsString(aNode: PVirtualNode;
   function LJsonVarToStr(const aValue: Variant): string;
   begin
     if VarIsBool(aValue) then
-      result := Utf8ToString(LowerCase(VariantToUtf8(aValue)))
+      result := Utf8ToString(LowerCase(aValue))
     else
       result := Utf8ToString(VariantToUtf8(aValue));
   end;