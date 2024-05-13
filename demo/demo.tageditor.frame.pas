8728c056651eebc7e9b6be078a46bc6efa336cfb
htouvet <htouvet@tranquil.it>
2024-05-13 14:10:39 +0200

    2024

--- demo/demo.tageditor.frame.pas
+++ demo/demo.tageditor.frame.pas
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------
 //    This file is part of Tranquil IT Software
-//    Copyright (C) 2012 - 2023  Tranquil IT https://www.tranquil.it
+//    Copyright (C) 2012 - 2024  Tranquil IT https://www.tranquil.it
 //    All Rights Reserved.
 // ------------------------------------------------------------------
 unit demo.tageditor.frame;
